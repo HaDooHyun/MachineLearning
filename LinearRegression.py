27634bb3394c92ad2a9b60671af3079549eeba2e
hadoohyun <hadoo8688@gmail.com>
2018-09-06 21:47:15 +0900

    Make Graph Shape And Insert Dataset

--- LinearRegression.py
+++ LinearRegression.py
@@ -10,26 +10,40 @@ b = np.random.uniform(0, 1)
 x = []
 y = []
 for repeat in range(10):
-    data_x = np.random.uniform(0, 1)
-    data_y = np.random.uniform(0, 1)
+    data_x = np.random.uniform(0.3, 0.7)
+    data_y = np.random.uniform(0.3, 0.7)
     x.append(data_x)
     y.append(data_y)
 
-for real_y in y:
-    print(real_y)
+def f(x) :
+    return a * x + b
 
-#def f(x) :
-#    return a * x + b
-#
-#def cost(x) :
-#    sum = 0
-#    for real_y in y:
-#        sum += (f(x) - real_y)**2
-#    return sum / len(y)
-#
-#frame = plt.figure()
-#
-#graph1 = frame.add_subplot(321)
-#grpha2 = frame.add_subplot(322)
-#graph3 = frame.add_subplot(325)
-#graph4 = frame.add_subplot(326)
+def cost(x) :
+    sum = 0
+    for real_y in y:
+        sum += (f(x) - real_y)**2
+    return sum / len(y)
+
+def w(a) :
+    sum = 0
+    for repeat in range(10):
+        sum += 2 * x[repeat]**2 * a + 2 * (b - y[repeat]) * x[repeat]
+    return sum
+
+def bias(b) :
+    sum = 0
+    for repeat in range(10):
+        sum += 2 * b + 2 * a
+    return sum
+
+frame = plt.figure()
+
+graph1 = frame.add_subplot(321)
+grpha2 = frame.add_subplot(322)
+graph3 = frame.add_subplot(325)
+graph4 = frame.add_subplot(326)
+
+graph1.axis([0,1,0,1])
+graph1.scatter(x, y)
+
+plt.show()
